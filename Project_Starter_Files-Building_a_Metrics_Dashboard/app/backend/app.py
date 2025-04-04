afd4b3fd360c830b357ab50e4d2736adcaeb70fb
Alexander Placidi <a.placidi@posteo.de>
2025-04-04 14:47:54 +0200

    wip

---
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/app/backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+app.config["MONGO_DBNAME"] = "example-mongodb"
+app.config[
+    "MONGO_URI"
+] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def homepage():
+    return "Hello World"
+
+@app.route("/star", methods=["POST"])
+def add_star():
+    star = mongo.db.stars
+    name = request.json["name"]
+    distance = request.json["distance"]
+    star_id = star.insert({"name": name, "distance": distance})
+    new_star = star.find_one({"_id": star_id})
+    output = {"name": new_star["name"], "distance": new_star["distance"]}
+    return jsonify({"result": output})
+
+if __name__ == "__main__":
+    app.run()
