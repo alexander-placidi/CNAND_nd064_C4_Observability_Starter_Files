55ce8f3a66c864e901836ac4329bb88f4b652ac9
Alexander Placidi <a.placidi@posteo.de>
2025-04-07 23:01:56 +0200

    wip

--- Project_Starter_Files-Building_a_Metrics_Dashboard/app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/app/backend/app.py
@@ -1,4 +1,4 @@
-import logging
+import logging, requests
 from flask import Flask, request, jsonify
 from jaeger_client import Config
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
@@ -34,12 +34,36 @@ def init_tracer(service):
 
 tracer = init_tracer("backend")
 
+NASA_API_KEY = "DEMO_KEY"
+
 @app.route("/")
 def homepage():
-    with tracer.start_span('say-hello') as span:
-        span.set_tag('test-tag', "test")
+    with tracer.start_span('get_asteroids') as span:
+        res = requests.get(f"https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key={NASA_API_KEY}")
+        json_response = res.json()
+        near_earth_objects_dict = json_response["near_earth_objects"]
+        span.set_tag("asteroids_count", json_response["element_count"])
+        asteroids = []
+        
+        for k, v in near_earth_objects_dict.items():
+            logging.info(f"Getting near earth asteriods {k}")
+            for near_earth_object in v:
+                asteroid = {}
+                
+                with tracer.start_span("request-Near_Earth_Object_Web_Service") as site_span:
+                    logging.info(f"Name: {near_earth_object["name"]} Hazardous: {near_earth_object["is_potentially_hazardous_asteroid"]}")
+                    try:
+                        asteroid["name"] = near_earth_object["name"]
+                        asteroid["is_potentially_hazardous_asteroid"] = near_earth_object["is_potentially_hazardous_asteroid"]
+                        asteroids.append(asteroid)
+                        site_span.set_tag("http.status_code", res.status_code)
+                        site_span.set_tag("asteriod_name", asteroid["name"])
+                        site_span.set_tag("is_potentially_hazardous_asteroid", asteroid["is_potentially_hazardous_asteroid"])
+                    except Exception:
+                        logging.error(f"Unable to get asteroid info")
+                        site_span.set_tag("http.status_code", res.status_code)      
     
-    return "Hello World"
+    return asteroids
 
 if __name__ == "__main__":
     app.run()
